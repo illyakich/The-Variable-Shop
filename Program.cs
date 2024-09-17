ddc6fb578a3611164dd14aa6243a483d37028b11
ПК <@DESKTOP-5UNSHBK>
2024-09-17 19:39:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿String Majima = "Goro";
+int murders = 0;
+short me = 170;
+long Stas = 185;
+byte bite = 21;
+sbyte sbite  = 22;
+uint homie = 20;
+ushort OrIsIt  = 12;
+ulong yeah = 13;
+char joeBiden = 'A';
+float siga = 32;
+double diouble = 21;
+decimal sax = 25.5M;
+bool True = false;
+
+Majima = "The GOAT";
+murders = 1;
+me = 171; // on a good day
+Stas = 186; // on a bad day (for me)
+bite = 22;
+sbite = 23;
+homie = 21;
+OrIsIt = 13;
+yeah = 156;
+joeBiden = 'B';
+siga = 31;
+diouble = 234;
+sax = 26.5M;
+True = True;
+
+Console.WriteLine($"{Majima} {murders} {me} {Stas} {bite} {sbite} {homie} {OrIsIt} {yeah} {joeBiden} {siga} {diouble} {sax} {True} ");
